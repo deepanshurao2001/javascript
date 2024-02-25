b0befaf9922016752f3ad9ae69d454e216d62554
deepanshurao2001 <106137206+deepanshurao2001@users.noreply.github.com>
2024-02-25 17:03:46 +0000

    conversion of DataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "33abc"
+console.log(score);
+ console.log(typeof score);
+ console.log(typeof(score));
+
+ let valueInNumber = Number(score)
+ console.log(valueInNumber);
+console.log(typeof valueInNumber);
+console.log(typeof(valueInNumber));
+
+console.log(typeof NnN);
+console.log(typeof(NnN));
+
+// "33" =>
+
+let a = "true"
+console.log(a);
+console.log(typeof a);
+
+let b = Number(a)
+console.log(b);
+console.log(typeof b);
+
+let isloggedIN = 7
+console.log(isloggedIN);
+console.log(typeof isloggedIN);
+
+let  booleanIsLoggedIn = Boolean(isloggedIN)
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
