b9c986a26c6d83c9462d9ef6964c22b1ab03da45
deepanshurao2001 <106137206+deepanshurao2001@users.noreply.github.com>
2024-03-02 18:25:38 +0000

    number and maths

---
+++ 01_basics/06nums_and_maths.js
@@ -0,0 +1,11 @@
+const score = 400
+console.log(score);
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(3));
+
+const otherNumber = 1123.8966
+
+console.log(otherNumber.toPrecision(4));
