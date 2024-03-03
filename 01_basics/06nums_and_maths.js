763130e07d290f53a93002ad33f26d8e21d08937
deepanshurao2001 <106137206+deepanshurao2001@users.noreply.github.com>
2024-03-03 17:03:19 +0000

    maths and Date

--- 01_basics/06nums_and_maths.js
+++ 01_basics/06nums_and_maths.js
@@ -6,6 +6,8 @@ console.log(balance);
 console.log(balance.toString().length);
 console.log(balance.toFixed(3));
 
-const otherNumber = 1123.8966
+const otherNumber = 1123.8966  
 
 console.log(otherNumber.toPrecision(4));
+
+console.log(Math.round(4.5));
