806c5300ce8eb5d3af376bcc2ec7aa79b41f7480
deepanshurao2001 <106137206+deepanshurao2001@users.noreply.github.com>
2024-03-09 16:42:45 +0000

    object in javascript

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -19,3 +19,30 @@ const regularUser ={
 } 
 
 console.log(regularUser.fullName.userfullname.lastname,regularUser.fullName.userfullname.firstname);
+
+const users = [
+    {
+        id: 1,
+        email: "abc@gmail.com"
+    },
+    {
+        id: 1,
+        email: "xyz@gmail.com"
+    },
+    {
+        id: 1,
+        email: "mno@gmail.com"
+    }
+]
+
+console.log(users[0].email);
+console.log(tinderUser);
+//we can only display only keys of objects
+console.log(Object.keys(tinderUser));
+// we  can store the keys of above object in a seprate object
+const keysOnly = Object.keys(tinderUser)
+
+console.log(typeof keysOnly);
+
+console.log(keysOnly);
+
