3dc0e956140c3ea78177f56f7623ebe816cbaf80
deepanshurao2001 <106137206+deepanshurao2001@users.noreply.github.com>
2024-03-13 10:51:43 +0000

    functions and returns

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,5 +1,17 @@
 function  addTwoNumber(number1,number2){
+
     console.log(number1 + number2);
+
 }
 
-addTwoNumber(12,34)
+addTwoNumber(12,null)
+
+function subtractTwoNumber(number1,number2){
+    if(number1 >= number2){
+        return `difference of numbers is ${number1 - number2}`
+    }
+
+    return number2 - number1
+}
+
+console.log(subtractTwoNumber(88,77));
