58117411821416989dc4e6498003f412b17514c3
deepanshurao2001 <106137206+deepanshurao2001@users.noreply.github.com>
2024-03-13 20:05:09 +0000

    arrow function implecit return and explecit return and this

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,26 @@
+var c = 100
+if (true){
+    
+let a = 10 
+const b = 20
+var c = 30
+
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c);
+
+//+++++++++++++++++++++++++interesting++++++++++++++++++++++++++++
+
+function addone(num){
+    return num + 1
+}
+
+addone(5)
+
+const addtwo = function(num){
+    return num + 2
+}
+
+addtwo(5)
---
+++ 03_basics/03Arrow.js
@@ -0,0 +1,19 @@
+const user = {
+    username: "Deepanshu",
+    price: 999,
+
+    welcomeMessage: function(){
+        console.log(`${this.username}, welcome to website`);
+    }
+}
+
+user.welcomeMessage()
+// const addTwo = (num1,num2) => {username: "Deepanshu"}
+
+// const addTwo = (num1, num2) => [username = "Deepanshu" , age = 14]
+
+// const addTwo = (num1, num2) => ({username: "Deepanshu"})
+
+const addTwo = (num1, num2) => num1 + num2
+
+console.log(addTwo(3, 4))
